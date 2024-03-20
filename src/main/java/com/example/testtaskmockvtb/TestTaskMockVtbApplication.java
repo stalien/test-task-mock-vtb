9a0d79247c318c4ad2c1fed86c169a791e16d77f
stalien <stalien@yandex.ru>
2024-03-20 17:26:28 +0300

    Initial commit

---
+++ src/main/java/com/example/testtaskmockvtb/TestTaskMockVtbApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtaskmockvtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskMockVtbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskMockVtbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testtaskmockvtb/TestTaskMockVtbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtaskmockvtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskMockVtbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
